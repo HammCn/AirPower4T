0408192bdf65a3e34fae7bbc30d6f4f565b31a65
Hamm <majhamm@qq.com>
2023-06-25 17:09:21 +0800

    fix(bugs): 修复了一些已知的BUG

--- model/entity/AirFileEntity.ts
+++ model/entity/AirFileEntity.ts
@@ -1,16 +1,10 @@
-import { AirEntity } from '@/airpower/base/AirEntity'
-import { IFile } from '@/airpower/interface/IFile'
+import { AirEntity } from '../../base/AirEntity'
+import { IFile } from '../../interface/IFile'
 
 /**
- * # 内置文件实体
- * ---
- * 💡 如需扩展, 请自行实现 ```IFile```, 使用 ```AirConfig.defaultFileEntity``` 配置默认实现类
+ * # 内置的文件类
  * @author Hamm
  */
 export class AirFileEntity extends AirEntity implements IFile {
-  /**
-   * # 文件的相对路径
-   * 可以调用 ```AirFile.getStaticFileUrl()``` 获取文件的真实地址
-   */
   url!: string
 }
