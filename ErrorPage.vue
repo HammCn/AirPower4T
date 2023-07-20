b98a1dc7b225947d11c19f485318642707d70866
Hamm <majhamm@qq.com>
2023-07-20 21:42:14 +0800

    fix(bugs): 修复了一些已知的BUG

--- ErrorPage.vue
+++ ErrorPage.vue
@@ -14,12 +14,12 @@
         {{ errorTitle }}
       </div>
       <div class="description">
-        {{ errorDesc }}
+        {{ errorDesc }} <router-link to="/">
+          返回首页
+        </router-link>
       </div>
     </div>
-    <div
-      class="percent"
-    >
+    <div class="percent">
       <div />
     </div>
   </div>
@@ -53,30 +53,26 @@ watch(errorCode, () => {
   switch (errorCode.value) {
     case AirHttpStatus.FORBIDDEN:
       errorTitle.value = '很抱歉,你可能无权访问此服务'
-      errorDesc.value = '请确认你的身份权限,你可以联系客服或管理员解决此问题'
+      errorDesc.value = '请确认你的身份权限, '
       break
     case AirHttpStatus.INTERNAL_SERVER_ERROR:
       errorTitle.value = '很抱歉,服务器发生了一点小错误'
-      errorDesc.value = '服务器可能正在维护中,你可以稍后再试或联系客服或管理员'
+      errorDesc.value = '服务器可能正在维护中, '
       isError.value = true
       break
     case AirHttpStatus.NOT_FOUND:
       errorTitle.value = '很抱歉,没有找到你想访问的资源'
-      errorDesc.value = '请确认你的访问地址是否正确, 你可以联系客服或管理员解决此问题'
+      errorDesc.value = '请确认你的访问地址是否正确, 或 '
       break
     case AirHttpStatus.GATEWAY_TIMEOUT:
       errorTitle.value = '很抱歉,服务可能依然还没有正确的响应.'
-      errorDesc.value = '建议尝试检查你的网络后刷新重试或联系客服或管理员解决此问题'
+      errorDesc.value = '建议尝试检查你的网络后刷新重试或 '
       break
     default:
   }
   if (errorCode.value !== AirHttpStatus.OK) {
     isLoading.value = false
     isError.value = true
-    setTimeout(() => {
-      // eslint-disable-next-line no-restricted-globals
-      location.replace('/')
-    }, 5000)
   }
 })
 
@@ -151,7 +147,7 @@ watch(() => route, () => {
     }
 
     .title {
-      font-size: 24px;
+      font-size: 20px;
       color: #333;
     }
 
@@ -159,6 +155,16 @@ watch(() => route, () => {
       font-size: 14px;
       color: #ccc;
       margin-top: 10px;
+
+      a {
+        color: var(--el-color-primary);
+        text-decoration: none;
+      }
+
+      a:hover {
+        text-decoration: underline;
+        font-weight: bold;
+      }
     }
 
   }
--- model/entity/AirMenuEntity.ts
+++ model/entity/AirMenuEntity.ts
@@ -34,4 +34,43 @@ export class AirMenuEntity extends AirEntity implements IMenu {
    * # 菜单是否隐藏
    */
   @Type(Boolean) isHide = false
+
+  /**
+   * # 设置菜单名称
+   * @param name 菜单名称
+   */
+  setName(name: string): this {
+    this.name = name
+    return this
+  }
+
+  /**
+   * # 设置菜单路径
+   * ---
+   * ### 💡 同步设置 ```component``` 如需手动设置 请继续调用 ```setComponent()```
+   * @param path 路径
+   */
+  setPath(path: string): this {
+    this.path = path
+    this.component = path
+    return this
+  }
+
+  /**
+   * # 设置组件地址
+   * @param component 组件地址
+   */
+  setComponent(component: string): this {
+    this.component = component
+    return this
+  }
+
+  /**
+   * # 设置子菜单列表
+   * @param children 子菜单列表
+   */
+  setChildren(children: this[]): this {
+    this.children = children
+    return this
+  }
 }
