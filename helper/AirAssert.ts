2bdaa3877d6b6a0b184fe6059e54288e2cf03c88
Hamm <majhamm@qq.com>
2023-07-31 10:42:53 +0800

    feat(Assert): 添加一个简单断言类

---
+++ helper/AirAssert.ts
@@ -0,0 +1,37 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { AirNotification } from '../feedback/AirNotification'
+
+export class AirAssert {
+  /**
+   *
+   * @param condition 条件
+   * @param message 错误信息
+   * @param title [可选]错误标题
+   */
+  static when(condition: boolean, message: string, title?: string) {
+    if (condition) {
+      AirNotification.warning(message, title)
+      throw new Error(`\n\n[AirAssert Faild]: ${title || ''}\n${message}\n\n\n`)
+    }
+  }
+
+  /**
+   * # 数据是否为 ```null```
+   * @param value 断言的值
+   * @param message 错误信息
+   * @param title [可选]错误标题
+   */
+  static whenNull(value: any, message: string, title?: string) {
+    return this.when(value === null, message, title)
+  }
+
+  /**
+   * # 数据是否为 ```undefined```
+   * @param value 断言的值
+   * @param message 错误信息
+   * @param title [可选]错误标题
+   */
+  static whenUndefined(value: any, message: string, title?: string) {
+    return this.when(value === undefined, message, title)
+  }
+}
