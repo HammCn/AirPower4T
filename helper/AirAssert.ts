2ad48713903dbb79c7538d32a8e881b8dff55ef6
Hamm <majhamm@qq.com>
2023-08-01 21:44:31 +0800

    fix(bugs): 修复了一些已知的BUG

--- helper/AirAssert.ts
+++ helper/AirAssert.ts
@@ -1,17 +1,17 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { AirNotification } from '../feedback/AirNotification'
+import { AirAlert } from '../feedback/AirAlert'
 
 export class AirAssert {
   /**
    * # 断言条件成立时抛出异常
    * @param condition 条件
    * @param message 错误信息
-   * @param title [可选]错误标题
+   * @param description [可选]错误描述
    */
-  static when(condition: boolean, message: string, title?: string) {
+  static when(condition: boolean, message: string, description?: string) {
     if (condition) {
-      AirNotification.warning(message, title)
-      throw new Error(`\n\n[AirAssert Faild]: ${title || ''}\n${message}\n\n\n`)
+      AirAlert.show(message, description)
+      throw new Error(`\n\n[AirAssert Faild]: ${message || ''}\n${description}\n\n\n`)
     }
   }
 
@@ -19,19 +19,19 @@ export class AirAssert {
    * # 数据是否为 ```null```
    * @param value 断言的值
    * @param message 错误信息
-   * @param title [可选]错误标题
+   * @param description [可选]错误描述
    */
-  static whenNull(value: any, message: string, title?: string) {
-    return this.when(value === null, message, title)
+  static whenNull(value: any, message: string, description?: string) {
+    return this.when(value === null, message, description)
   }
 
   /**
    * # 数据是否为 ```undefined```
    * @param value 断言的值
    * @param message 错误信息
-   * @param title [可选]错误标题
+   * @param description [可选]错误描述
    */
-  static whenUndefined(value: any, message: string, title?: string) {
-    return this.when(value === undefined, message, title)
+  static whenUndefined(value: any, message: string, description?: string) {
+    return this.when(value === undefined, message, description)
   }
 }
