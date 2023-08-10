e4b9d767f57e0e62c74fe7d3facafdf9fb205e18
Hamm <majhamm@qq.com>
2023-08-10 21:35:43 +0800

    fix(Validator): 修改了一个错误的注释

--- helper/AirValidator.ts
+++ helper/AirValidator.ts
@@ -311,7 +311,7 @@ export class AirValidator {
   }
 
   /**
-   * # 如果不是手机号时报错
+   * # 如果不是邮箱时报错
    */
   ifNotEmail(): this {
     this.validator = (_: any, value: string, callback: Function) => {
@@ -618,7 +618,7 @@ export class AirValidator {
         if (day > 31 || month < 1) {
           return false
         }
-        const arr: Array<Array<unknown>> = [[7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2], [1, 0, 'X', 9, 8, 7, 6, 5, 4, 3, 2]]
+        const arr: Array<Array<number | 'X'>> = [[7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2], [1, 0, 'X', 9, 8, 7, 6, 5, 4, 3, 2]]
         let sum = 0
         for (let i = 0; i < 17; i += 1) {
           sum += parseInt(str[i], this.DEFAULT_RADIX) * (arr[0][i] as number)
