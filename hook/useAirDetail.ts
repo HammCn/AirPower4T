62979e5f0f2ff8099787309341de5418039f7d69
Hamm <majhamm@qq.com>
2023-08-14 11:26:13 +0800

    feat(Hook): 提供了一个标准Detail的Hook

---
+++ hook/useAirDetail.ts
@@ -0,0 +1,45 @@
+import { Ref, computed, ref } from 'vue'
+import { IUseDetailOption } from '../interface/IUseDetailOption'
+import { IUseDetailResult } from '../interface/IUseDetailResult'
+import { AirEntity } from '../base/AirEntity'
+import { ClassConstructor } from '../type/ClassConstructor'
+import { AirAbstractEntityService } from '../base/AirAbstractEntityService'
+import { AirClassTransformer } from '../helper/AirClassTransformer'
+
+/**
+ * # 引入详情的Hook
+ * @param props defineProps的返回值
+ * @param entityClass Editor使用的实体类
+ * @param serviceClass Editor使用的Service
+ * @param option [可选]更多的配置
+ */
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function useAirDetail<E extends AirEntity>(props: any, entityClass: ClassConstructor<E>, serviceClass: ClassConstructor<AirAbstractEntityService<E>>, option: IUseDetailOption<E> = {}): IUseDetailResult<E> {
+  const isLoading = ref(false)
+
+  const service = AirClassTransformer.newInstance(serviceClass)
+  service.loading = isLoading
+
+  const formData: Ref<E> = ref(props.param.copy())
+
+  async function getDetail() {
+    if (props.param.id) {
+      formData.value = await service.getDetail(props.param.id)
+
+      if (option.afterGetDetail) {
+        const result = option.afterGetDetail(formData.value)
+        if (result !== undefined) {
+          formData.value = result
+        }
+      }
+    }
+  }
+
+  getDetail()
+
+  const title = computed(() => `${formData.value.getClassName()}详情`)
+
+  return {
+    title, formData, isLoading,
+  } as IUseDetailResult<E>
+}
