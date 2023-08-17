6f36b2d59cc3191cc340b47c05279998c59c2d12
Hamm <majhamm@qq.com>
2023-08-17 10:52:35 +0800

    feat(Websocket): 支持了一个Websocket服务类

--- websocket/AirWebSocket.ts
+++ websocket/AirWebSocket.ts
@@ -2,13 +2,30 @@ import { AirConfig } from '../config/AirConfig'
 import { AirAlert } from '../feedback/AirAlert'
 
 export class AirWebsocket {
+  /**
+   * # WebSocket实例
+   */
   websocket!: WebSocket
 
+  /**
+   * # 是否已连接
+   */
   isConnected = false
 
-  heartBeatTimer!: number
+  /**
+   * # 同步Timer
+   */
+  private heartBeatTimer!: number
 
-  heartBeatSecond = 5
+  /**
+   * # 心跳秒
+   */
+  private heartBeatSecond = 30
+
+  /**
+   * # 自动重连
+   */
+  private reconnectWhenClosed = true
 
   /**
    * # 设置Websocket心跳秒
@@ -33,10 +50,21 @@ export class AirWebsocket {
   }
 
   /**
-   * # 创建一个WebSocket
-   * @param url [可选]URL
+   * # 是否自动重连
+   * @param autoConnectWhenClose 是否自动重连
    */
-  static create(): AirWebsocket {
+  private autoConnectWhenClosed(autoConnectWhenClose = true) {
+    this.reconnectWhenClosed = autoConnectWhenClose
+  }
+
+  /**
+   * # 创建一个WebSocket
+   */
+  static create(handler: {
+    // eslint-disable-next-line no-unused-vars
+    onmessage?: (message: string) => void,
+    onopen?: () => void
+  }): AirWebsocket {
     if (!AirConfig.websocketUrl) {
       AirAlert.error('请配置环境变量 VITE_APP_WEBSOCKET_URL')
       return new AirWebsocket()
@@ -46,10 +74,26 @@ export class AirWebsocket {
     instance.websocket.onopen = () => {
       instance.isConnected = true
       instance.startHeartBeat()
+      if (handler.onopen) {
+        handler.onopen()
+      }
+    }
+    instance.websocket.onmessage = (message) => {
+      if (handler.onmessage) {
+        handler.onmessage(message.data)
+      }
     }
     instance.websocket.onclose = () => {
       instance.isConnected = false
     }
     return instance
+  }
+
+  /**
+   * # 关闭WebSocket
+   */
+  close() {
+    this.reconnectWhenClosed = false
+    this.websocket.close()
   }
 }
