23c2df688fc195a6403b02a1a18710e2a998c9b4
Hamm <majhamm@qq.com>
2023-09-25 15:43:15 +0800

    fix(Permission): 格式化代码

--- enum/AirPermissionAction.ts
+++ enum/AirPermissionAction.ts
@@ -8,34 +8,42 @@ export enum AirPermissionAction {
    * # 添加
    */
   ADD = 'add',
+
   /**
    * # 修改
    */
   EDIT = 'update',
+
   /**
    * # 删除
    */
   DELETE = 'delete',
+
   /**
    * # 详情
    */
   DETAIL = 'getDetail',
+
   /**
    * # 添加下级数据
    */
   ADD_CHILD = 'addChild',
+
   /**
    * # 导出
    */
   EXPORT = 'export',
+
   /**
    * # 导入
    */
   IMPORT = 'import',
+
   /**
    * # 导入
    */
   DISABLE = 'disable',
+
   /**
    * # 导入
    */
