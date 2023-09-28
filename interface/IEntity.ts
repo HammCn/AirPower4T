1c7bf4386dc2349a4dea9009420295dace402119
Hamm <majhamm@qq.com>
2023-09-28 11:20:04 +0800

    fix(IEntity): 添加继承至AirModel

--- interface/IEntity.ts
+++ interface/IEntity.ts
@@ -1,8 +1,10 @@
+import { AirModel } from '../base/AirModel'
+
 /**
  * # 标准实体接口
  * @author Hamm
  */
-export interface IEntity {
+export interface IEntity extends AirModel{
   /**
    * # 主键ID
    */
