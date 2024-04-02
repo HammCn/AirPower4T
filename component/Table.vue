85cd058269e5268abcbd07686bb282417d50f3e5
Hamm <majhamm@qq.com>
2024-04-02 15:35:47 +0800

    fix(Table): 更多按钮的间距调整

--- component/Table.vue
+++ component/Table.vue
@@ -1298,6 +1298,7 @@ const isDetailShowInline = computed(() => {
   flex-direction: column;
   align-items: flex-start;
   justify-content: flex-start;
+  min-width: 120px;
 
   .air-button {
     width: 100%;
