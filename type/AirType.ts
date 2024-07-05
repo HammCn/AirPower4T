fa5ede0f2f46a86a5518eba3db553b85d07270a0
Hamm <majhamm@qq.com>
2024-07-05 22:43:59 +0800

    refactor(type): remove deprecated types from AirType.ts

--- type/AirType.ts
+++ type/AirType.ts
@@ -15,23 +15,3 @@ export type AirColorString = AirColor | string
  * # 状态码
  */
 export type AirCodeNumber = AirCode | number
-
-/**
- * # 表格对齐方式
- */
-export type AirTableAlign = 'left' | 'center' | 'right'
-
-/**
- * # 表格固定位置
- */
-export type AirTableFixed = 'left' | 'right'
-
-/**
- * # 表格是否可排序(排序方式)
- */
-export type AirSortable = boolean | 'custom'
-
-/**
- * #  图标类型字符串类型
- */
-export type AirIconType = 'DELETE_LIST' | 'DELETE' | 'EDIT' | 'DETAIL' | 'ADD' | 'FINISH' | 'SEARCH' | 'SETTING' | 'PUBLISH' | 'SAVE' | 'EXPORT' | 'IMPORT' | 'MORE' | 'LOCK' | 'UPLOAD' | 'LOGOUT' | 'DOWNLOAD' | 'CHECKIN' | 'SELECT' | 'MOVE_UP' | 'MOVE_DOWN' | 'CONFIRM' | 'CHECKBOX' | 'CLOCK' | 'MONITOR' | 'COMPARE' | 'CLOSE'
