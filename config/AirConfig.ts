00e78696486c77dc34aba477d59590b96fa94673
Hamm <majhamm@qq.com>
2024-08-05 16:59:35 +0800

    fix(bugs): 修复了一些已知的BUG

--- config/AirConfig.ts
+++ config/AirConfig.ts
@@ -4,109 +4,102 @@ import { AirApi } from './AirApi'
 import { AirCodeNumber } from '../type/AirType'
 
 /**
- * # AirPower 全局配置
- * ---
- * 💡 可自行在 ```main.ts``` 中覆盖此类中的配置
+ * # `AirPower` 全局配置
+ * 可自行在 `main.ts` 中覆盖此类中的配置
  * @author Hamm.cn
  */
 export class AirConfig {
   /**
-   * # AirPower版本号
+   * ## `AirPower` 版本号
    */
   static readonly version = 'v2.1.3'
 
   /**
-   * # AppKey
-   * ---
-   * 💡 用于处理一些唯一场景做项目区分 以及Oauth2的AppKey
+   * ## `AppKey`
+   * 用于处理一些唯一场景做项目区分 以及 `Oauth2` 的 `AppKey`
    */
   static appKey = 'airpower'
 
   /**
-   * # AppKey Header
+   * ## `AppKey Header`
    */
   static appKeyHeader = 'appkey'
 
   /**
-   * # 项目名称
-   * ---
-   * 💡 会显示在浏览器标题上
+   * ## 项目名称
    */
   static product = ''
 
   /**
-   * # 接口根地址
-   * ---
-   * ### 💡 以 ```/``` 结尾
+   * ## 接口根地址
+   * 以 `/` 结尾
    */
   static apiUrl = ''
 
   /**
-   * # 静态资源根路径
-   * ---
-   * ### 💡 以 ```/``` 结尾
+   * ## 静态资源根路径
+   * 以 `/` 结尾
    */
   static staticUrl = ''
 
   /**
-   * # 默认的文件上传地址
+   * ## 默认的文件上传地址
    */
   static uploadUrl = `${AirConfig.apiUrl}attach/upload`
 
   /**
-   * # 上传文件默认字段名称
+   * ## 上传文件默认字段名称
    */
   static uploadFileName = 'file'
 
   /**
-   * # AccessToken对应的Key
-   * ---
-   * 💡 ```缓存的名称``` 和 ```Api传输的Header``` 都叫这个名字
+   * ## `AccessToken` 对应的 `Key`
+   * `缓存的名称` 和 `Api传输的Header` 都叫这个名字
    */
   static authorizationHeaderKey = 'Authorization'
 
   /**
-   * # Http返回状态码的字段
+   * ## `Http` 返回状态码的字段
    */
   static httpCodeKey = 'code'
 
   /**
-   * # Http返回错误信息的字段
+   * ## `Http` 返回错误信息的字段
    */
   static httpMessageKey = 'message'
 
   /**
-   * # Http返回数据的字段
+   * ## `Http` 返回数据的字段
    */
   static httpDataKey = 'data'
 
   /**
-   * # 全局http请求返回 成功状态码
+   * ## 全局 `http` 请求返回 成功状态码
    */
   static successCode: AirCodeNumber = AirCode.SUCCESS
 
   /**
-   * # 全局http请求返回 继续状态码
+   * ## 全局 `http` 请求返回 继续状态码
    */
   static continueCode: AirCodeNumber = AirCode.CONTINUE
 
   /**
-   * # 全局http请求返回 登录状态码
+   * ## 全局 `http` 请求返回 登录状态码
    */
   static unAuthorizeCode: AirCodeNumber = AirCode.UNAUTHORIZED
 
   /**
-   * # 权限列表
+   * ## 权限列表
    */
   private static permissionList: string[] = []
 
   /**
-   * # 权限缓存Key
+   * ## 权限缓存 `Key`
    */
   private static readonly permissionKey = '_permissions'
 
   /**
-   * # 保存权限列表
+   * ## 保存权限列表
    * @param permissions 权限列表
    */
   static savePermissionList(permissions: string[]) {
@@ -115,7 +108,7 @@ export class AirConfig {
   }
 
   /**
-   * # 获取缓存的权限列表
+   * ## 获取缓存的权限列表
    */
   static getPermissionList(): string[] {
     const str = AirApi.getStorage(this.appKey + this.permissionKey) || '[]'
@@ -127,7 +120,7 @@ export class AirConfig {
   }
 
   /**
-   * # 是否有权限
+   * ## 是否有权限
    * @param permission 权限标识
    */
   static hasPermission(permission: string): boolean {
@@ -135,7 +128,7 @@ export class AirConfig {
   }
 
   /**
-   * # 保存身份令牌
+   * ## 保存身份令牌
    * @param accessToken 身份令牌
    */
   static saveAccessToken(accessToken: string): void {
@@ -143,34 +136,33 @@ export class AirConfig {
   }
 
   /**
-   * # 获取身份令牌
+   * ## 获取身份令牌
    */
   static getAccessToken(): string {
     return AirApi.getStorage(this.authorizationHeaderKey)
   }
 
   /**
-   * # 移除本地存储的身份令牌
+   * ## 移除本地存储的身份令牌
    */
   static removeAccessToken(): void {
     AirApi.removeStorage(this.authorizationHeaderKey)
   }
 
   /**
-   * # 默认的格式化时间
-   * ---
-   * 💡 ```ADateTime``` ```ATable``` 的格式化都将默认使用这个配置
+   * ## 默认的格式化时间
+   * `ADateTime` `ATable` 的格式化都将默认使用这个配置
    * ```
    */
   static dateTimeFormatter = AirDateTimeFormatter.YYYY_MM_DD_HH_mm_ss
 
   /**
-   * # 网络请求失败最大重试次数
+   * ## 网络请求失败最大重试次数
    */
   static retryTimesWhenNetworkError = 3
 
   /**
-   * # 跳转登录的方法
+   * ## 跳转登录的方法
    */
   static login = () => {
     AirApi.redirect('/view/login')
