8599a6743bf912a0c5010923606e32108a4d928b
Hamm <majhamm@qq.com>
2024-08-27 16:26:02 +0800

    style(Page): 优化了分页组件的样式

--- component/Page.vue
+++ component/Page.vue
@@ -252,6 +252,9 @@ const pageBoxWidth = [230, 230, 230, 230, 280, 340, 370, 400, 430]
       color: var(--el-color-primary);
     }
   }
+  .air-page-count{
+    border-radius: 3px;
+  }
 
   .air-page-count,
   .air-page-total {
