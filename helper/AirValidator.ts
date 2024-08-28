973e0aaadb883973b4f8eaa95e814a59246dc186
Hamm <majhamm@qq.com>
2024-08-28 22:08:14 +0800

    fix(Validator): 优化身份证验证逻辑

--- helper/AirValidator.ts
+++ helper/AirValidator.ts
@@ -159,9 +159,9 @@ export class AirValidator {
 
     if (str.length === 15) {
       // 15位省份证校验
-      if (/^[1-9]\d{5}((\d{2}(((0[13578]|1[02])(0[1-9]|[12][0-9]|3[01]))|((0[13456789]|1[012])(0[1-9]|[12][0-9]|30))|(02(0[1-9]|1[0-9]|2[0-8]))))|(((0[48]|[2468][048]|[13579][26])|(00))0229))\d{2}[0-9Xx]$/.test(str)) {
-        return true
-      }
+      return /^[1-9]\d{5}((\d{2}(((0[13578]|1[02])(0[1-9]|[12][0-9]|3[01]))|((0[13456789]|1[012])(0[1-9]|[12][0-9]|30))|(02(0[1-9]|1[0-9]|2[0-8]))))|(((0[48]|[2468][048]|[13579][26])|(00))0229))\d{2}[0-9Xx]$/.test(str)
+    }
+    if (str.length !== 18) {
       return false
     }
 
@@ -182,10 +182,7 @@ export class AirValidator {
       sum += parseInt(str[i], this.DEFAULT_RADIX) * (validArray[0][i] as number)
     }
     // eslint-disable-next-line eqeqeq
-    if (validArray[1][(sum % 11)] == str[17]) {
-      return true
-    }
-    return false
+    return validArray[1][(sum % 11)] == str[17]
   }
 
   /**
