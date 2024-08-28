dddbf8585d8f6af30b61b22981860c47772a42a3
Hamm <majhamm@qq.com>
2024-08-28 18:35:23 +0800

    fix(base): 禁用AirEnum中的eslint no-unused-vars规则

--- base/AirEnum.ts
+++ base/AirEnum.ts
@@ -1,4 +1,3 @@
-/* eslint-disable no-unused-vars */
 import { AirColor } from '../enum/AirColor'
 import { IDictionary } from '../interface/IDictionary'
 import { AirDictionaryArray } from '../model/extend/AirDictionaryArray'
@@ -102,6 +101,7 @@ export class AirEnum<K extends AirEnumKey = number> implements IDictionary {
    * ## 将枚举转为数组
    * @returns 枚举数组
    */
+  // eslint-disable-next-line no-unused-vars
   static toArray<K extends AirEnumKey, E extends AirEnum<K>>(this: ClassConstructor<E>): E[] {
     return Object.values(this)
       .filter((item) => item instanceof this)
@@ -111,6 +111,7 @@ export class AirEnum<K extends AirEnumKey = number> implements IDictionary {
    * ## 将枚举转为字典
    * @returns 枚举字典
    */
+  // eslint-disable-next-line no-unused-vars
   static toDictionary<D extends IDictionary>(this: ClassConstructor<D>): AirDictionaryArray<D> {
     return AirDictionaryArray.createCustom<D>(Object.values(this)
       .filter((item) => item instanceof this))
