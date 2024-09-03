b5f5ab2b870dda1526fdeac15fa6599c2d5f40bf
Hamm <majhamm@qq.com>
2024-09-03 17:47:54 +0800

    feat(Constant): 支持了一个`AirConstant`静态常量类

---
+++ config/AirConstant.ts
@@ -0,0 +1,51 @@
+/**
+ * # `AirPower` 内置常量
+ *
+ * @author Hamm.cn
+ */
+export class AirConstant {
+  /**
+   * ## `http://`
+   */
+  static readonly PREFIX_HTTP = 'http://'
+
+  /**
+   * ## `https://`
+   */
+  static readonly PREFIX_HTTPS = 'https://'
+
+  /**
+   * ## 空字符串
+   */
+  static readonly EMPTY_STRING = ''
+
+  /**
+   * ## `Content-Type`
+   */
+  static readonly CONTENT_TYPE = 'Content-Type'
+
+  /**
+   * ## 半角问号 `?`
+   */
+  static readonly QUESTION_MARK = '?'
+
+  /**
+   * ## 连接符号 `&`
+   */
+  static readonly AND_MARK = '&'
+
+  /**
+   * ## `AirModel`
+   */
+  static readonly AIR_MODEL = 'AirModel'
+
+  /**
+   * ## 下划线 `_`
+   */
+  static readonly UNDER_LINE = '_'
+
+  /**
+   * ## 默认进制
+   */
+  static readonly DEFAULT_RADIX = 10
+}
