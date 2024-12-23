6cfa2156d72d3de9ea0e4cb42613cdcd0e6987f5
Hamm <majhamm@qq.com>
2024-12-23 12:18:56 +0800

    fix(bugs): 修复了一些已知的BUG

--- model/entity/AirMenuEntity.ts
+++ model/entity/AirMenuEntity.ts
@@ -11,22 +11,22 @@ export class AirMenuEntity extends AirEntity implements IMenu {
   name!: string
 
   /**
-   * # 菜单`URL`
+   * ### 菜单`URL`
    */
   path!: string
 
   /**
-   * # 菜单图标
+   * ### 菜单图标
    */
   icon!: string
 
   /**
-   * # 菜单绑定组件路径
+   * ### 菜单绑定组件路径
    */
   component!: string
 
   /**
-   * # 菜单是否隐藏
+   * ### 菜单是否隐藏
    */
   @Field({
     type: Boolean,
@@ -41,7 +41,7 @@ export class AirMenuEntity extends AirEntity implements IMenu {
     children: this[] = []
 
   /**
-   * # 设置菜单名称
+   * ### 设置菜单名称
    * @param name 菜单名称
    */
   setName(name: string): this {
@@ -50,7 +50,7 @@ export class AirMenuEntity extends AirEntity implements IMenu {
   }
 
   /**
-   * # 设置菜单路径
+   * ### 设置菜单路径
    * 同步设置 `component` 如需手动设置 请继续调用 `setComponent()`
    * @param path 路径
    */
@@ -61,7 +61,7 @@ export class AirMenuEntity extends AirEntity implements IMenu {
   }
 
   /**
-   * # 设置组件地址
+   * ### 设置组件地址
    * @param component 组件地址
    */
   setComponent(component: string): this {
@@ -70,7 +70,7 @@ export class AirMenuEntity extends AirEntity implements IMenu {
   }
 
   /**
-   * # 设置子菜单列表
+   * ### 设置子菜单列表
    * @param children 子菜单列表
    */
   setChildren(children: this[]): this {
