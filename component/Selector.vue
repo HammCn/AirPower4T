dccfd392fdebf0fe73911eea932f23ccca5854c6
Hamm <majhamm@qq.com>
2025-01-16 14:18:46 +0800

    style(component): 格式化 Selector 组件代码
    - 调整了代码缩进和空格
    - 修复了部分代码格式问题，提高代码可读性

--- component/Selector.vue
+++ component/Selector.vue
@@ -259,10 +259,10 @@ const searchParamList = computed(() => {
     list = props.searchParams
   }
   const defaultFilter = props.props.param
-  if(!defaultFilter){
+  if (!defaultFilter) {
     return list
   }
-  return list.filter((item) =>  !(defaultFilter[item?.key] !== null && defaultFilter[item?.key] !== undefined))
+  return list.filter((item) => !(defaultFilter[item?.key] !== null && defaultFilter[item?.key] !== undefined))
 })
 
 /**
