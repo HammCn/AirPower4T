cc4e9e46118ef1e0dbbb80c5dc82449824261d98
Hamm <majhamm@qq.com>
2025-04-02 16:09:11 +0800

    fix(bugs): 修复了一些已知的BUG

---
+++ vite.config.ts
@@ -0,0 +1,29 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import dts from 'vite-plugin-dts'
+
+export default defineConfig({
+  plugins: [
+    vue(),
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
+  build: {
+    lib: {
+      entry: './src/index.ts',
+      name: 'AirPower4T',
+      fileName: (format) => `airpower4t.${format}.js`,
+      formats: ['es', 'umd'],
+    },
+    rollupOptions: {
+      external: ['vue', 'element-plus'],
+      output: {
+        globals: {
+          vue: 'Vue',
+          'element-plus': 'ElementPlus',
+        },
+      },
+    },
+  },
+})
